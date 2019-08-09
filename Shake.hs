60683b5c87a4086b364c0a5f14307bb8853728fb
Dale Wijnand <dale.wijnand@gmail.com>
2019-08-09 10:40:06 +0100

    Simplify

--- Shake.hs
+++ Shake.hs
@@ -2,15 +2,14 @@ import Development.Shake
 import Development.Shake.FilePath
 
 main = shakeArgs shakeOptions $ do
-    let targets = [f <.> "txt" | f <- ["25K", "50K", "100K", "200K", "400K", "800K", "1.6M", "3.2M"]]
-    let values = ["250000","500000", "1000000", "2000000", "4000000", "8000000", "16000000", "32000000"]
+    let values = [x <> "0000" | x <- ["25", "50", "100", "200", "400", "800", "1600", "3200"]]
+    let targets = [f <.> "txt" | f <- values]
     phony "all" $ need targets
 
-    foldMap (\(t, v) -> t %> \out -> do
-            need ["Main"]
-            Stderr stderr <- cmd "./Main +RTS -s -RTS" v
-            writeFile' out stderr
-        ) (zip targets values)
+    "*0000.txt" %> \out -> do
+        need ["Main"]
+        Stderr stderr <- cmd "./Main +RTS -s -RTS" (takeBaseName out)
+        writeFile' out stderr
 
     "Main" %> \out -> do
         need ["Main.hs"]
