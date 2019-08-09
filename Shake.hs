064e398b37a784b7ef9be2d6e1e1b609b455fe22
Dale Wijnand <dale.wijnand@gmail.com>
2019-08-09 11:16:25 +0100

    Learn forM_

--- Shake.hs
+++ Shake.hs
@@ -1,15 +1,17 @@
+import Control.Monad
 import Development.Shake
 import Development.Shake.FilePath
 
 main = shakeArgs shakeOptions $ do
-    let values = [x <> "0000" | x <- ["25", "50", "100", "200", "400", "800", "1600", "3200"]]
-    let targets = [f <.> "txt" | f <- values]
+    let targets = [f <.> "txt" | f <- ["25K", "50K", "100K", "200K", "400K", "800K", "1.6M", "3.2M"]]
+    let values = ["250000","500000", "1000000", "2000000", "4000000", "8000000", "16000000", "32000000"]
     phony "all" $ need targets
 
-    "*0000.txt" %> \out -> do
-        need ["Main"]
-        Stderr stderr <- cmd "./Main +RTS -s -RTS" (takeBaseName out)
-        writeFile' out stderr
+    forM_ (zip targets values) $ \(t, v) ->
+        t %> \out -> do
+            need ["Main"]
+            Stderr stderr <- cmd "./Main +RTS -s -RTS" v
+            writeFile' out stderr
 
     "Main" %> \out -> do
         need ["Main.hs"]
